8a984960a371a363c895427c5831a29eca888b6d
Muriel Silveira <murielkong@gmail.com>
2020-01-21 00:40:03 +0100

    feat: update buttons to use flat button and support press states

--- lib/auth/widgets/buttons.dart
+++ lib/auth/widgets/buttons.dart
@@ -15,42 +15,36 @@ class Button extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String finalText = text == null ? '' : text;
-    Widget finalChild = child != null ? child : Text(finalText);
+    Widget finalChild = child == null ? Text(finalText) : child;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 6.0),
-      child: Container(
-        constraints: const BoxConstraints(
-          maxWidth: 275.0,
-          minHeight: 48.0,
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(6.0)),
-          boxShadow: [
-            BoxShadow(
-              color: Color.fromRGBO(157, 163, 180, 0.2),
-              offset: Offset(0.0, 1.5),
-              blurRadius: 6.5,
-            ),
-          ],
-        ),
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            child: Center(
-              child: finalChild,
-            ),
-            onTap: onPress,
+    return Container(
+      constraints: const BoxConstraints(
+        maxWidth: 275.0,
+        minHeight: 48.0,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(6.0),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(157, 163, 180, 0.2),
+            offset: Offset(0.0, 1.5),
+            blurRadius: 6.5,
           ),
+        ],
+      ),
+      child: FlatButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6.0),
         ),
+        child: Center(child: finalChild),
+        onPressed: onPress,
       ),
     );
   }
 }
 
 class PrimaryButton extends StatelessWidget {
-  // should text be a Text component and just the style be overwritten?
   const PrimaryButton(
     this.text, {
     Key key,
@@ -65,45 +59,39 @@ class PrimaryButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String finalText = text == null ? '' : text;
-    Widget finalChild = child == null
-        ? Text(finalText, style: TextStyle(color: Colors.white))
-        : child;
+    Widget finalChild = child == null ? Text(finalText) : child;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 6.0),
-      child: Container(
-        constraints: const BoxConstraints(
-          maxWidth: 275.0,
-          minHeight: 48.0,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(6.0)),
-          boxShadow: [
-            BoxShadow(
-              color: Color.fromRGBO(157, 163, 180, 0.1),
-              offset: Offset(0.0, 1.5),
-              blurRadius: 6.5,
-            ),
+    return Container(
+      constraints: const BoxConstraints(
+        maxWidth: 275.0,
+        minHeight: 48.0,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6.0),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(157, 163, 180, 0.1),
+            offset: Offset(0.0, 1.5),
+            blurRadius: 6.5,
+          ),
+        ],
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromRGBO(10, 196, 186, 1),
+            const Color.fromRGBO(43, 218, 142, 1),
           ],
-          gradient: LinearGradient(
-            colors: [
-              const Color.fromRGBO(10, 196, 186, 1),
-              const Color.fromRGBO(43, 218, 142, 1),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
         ),
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            splashColor: Colors.white.withAlpha(50),
-            child: Center(
-              child: finalChild,
-            ),
-            onTap: onPress,
-          ),
+      ),
+      child: FlatButton(
+        textColor: Colors.white,
+        splashColor: Colors.white.withAlpha(50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6.0),
         ),
+        child: Center(child: finalChild),
+        onPressed: onPress,
       ),
     );
   }
