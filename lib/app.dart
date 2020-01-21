9cf2d4b85c858e6a500f81c87a63a123cb171913
Muriel Silveira <murielkong@gmail.com>
2020-01-21 01:01:06 +0100

    wip

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:titan/auth/login_home_page.dart';
 import 'package:titan/bank/BankHome.dart';
 import 'package:titan/counter.dart';
+import 'package:titan/map/map_home.dart';
 import 'package:titan/sensazonal/home.dart';
 
 class App extends StatelessWidget {
@@ -13,11 +14,12 @@ class App extends StatelessWidget {
         primaryColor: const Color.fromARGB(255, 132, 19, 158),
       ),
       routes: {
-        '/': (context) => LoginHomePage(),
+        '/': (context) => MapHome(),
         '/login': (context) => LoginHomePage(),
         '/sensazonal': (context) => SensazonalHome(),
         '/counter': (context) => CounterPage(),
         '/bank': (context) => BankHome(),
+        '/map': (context) => MapHome(),
       },
     );
   }
